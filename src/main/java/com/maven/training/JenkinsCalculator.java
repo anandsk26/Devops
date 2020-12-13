88ca096f19c22bfe919420bf0c301fa1254b3c62
anandsk26 <anandsk26@gmail.com>
2020-12-13 06:12:35 -0500

    Added the code

---
+++ src/main/java/com/maven/training/JenkinsCalculator.java
@@ -0,0 +1,12 @@
+package com.maven.training;
+
+public class JenkinsCalculator {
+	// Method to add 2 numbers
+	public int addNumbers(int numberOne, int numberTwo) {
+		return numberOne + numberTwo;
+	}
+	// Method to subtract 2 numbers
+	public int subtractNumbers(int numberOne, int numberTwo) {
+		return numberOne - numberTwo;
+	}
+}
